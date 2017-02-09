42f86ec6b671046a87f2fd3c8bb0d72da58077ac
Arnaud Lemaire <alemaire@norad.fr>
2017-02-09 12:06:27 +0100

    up

--- examples/debian/aci-debian-cassandra/runlevels/build/10.install.sh
+++ examples/debian/aci-debian-cassandra/runlevels/build/10.install.sh
@@ -7,8 +7,10 @@ cat > /etc/apt/sources.list.d/cassandra.list<<EOF
 deb http://ftp.fr.debian.org/debian/ jessie main non-free contrib # needed for java8
 deb http://www.apache.org/dist/cassandra/debian 30x main
 deb-src http://www.apache.org/dist/cassandra/debian 30x main
-deb http://ftp.debian.org/debian jessie-backports main non-free contrib
+deb http://ftp.debian.org/debian jessie-backports main
 EOF
+
+apt-cache policy openjdk-8-jre-headless
 
 apt-get install -y --force-yes curl
 curl https://www.apache.org/dist/cassandra/KEYS | apt-key add -
